535f29c0aea1fc329216152e38a2ed37f0596a9c
Jacob Clarke <jacobclarke718@gmail.com>
2022-04-28 19:47:01 -0500

    added readme

--- R/scale_target.R
+++ R/scale_target.R
@@ -4,12 +4,13 @@ create_palette <- function(options) \(n) rep(options,n)[seq(n)]
 #' creates a shape and size scale for plotting targets in ggplot2
 #' @export 
 #' @importFrom ggplot2 discrete_scale
+#' @param size the size of the shapes
 #' @examples
 #' data.frame(x = rnorm(20), y = rnorm(20), z= factor(c(rep(1, 15), rep(2, 5)))) %>%
 #'    ggplot(aes(x = x, y = y, shape = z, size = z, color = z)) +
 #'    scale_target() +
 #'    geom_point()
-scale_target <- function(..., size = 1) {
+scale_target <- function(..., size = 2) {
   list(discrete_scale("shape", "shape_de", create_palette(c("\u2B24", "\u29BF")), ...),
-    discrete_scale("size", "size_de", create_palette(c(1 + size, 1 + size))))
+    discrete_scale("size", "size_de", create_palette(c(size, 7 + size))))
 }
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
+<div style="display: flex; justify-content: center;"><img href="https://quartzsoftware.com" src="https://quartzsoftware-assets.s3.amazonaws.com/logo.svg" height="100"/></div>
+
 # ggquartz
+
+Add a target scale to your plots.
+## Installation 
+
+```R
+remotes::install_github("quartzsoftwarellc/ggquartz")
+```
+
+## Usage
+
+`scale_target()` adds an alternating shape scale of a filled circle and a target and a alternating size scale for the size of the circles and targets.
+
+```R
+data.frame(x = rnorm(20), y = rnorm(20), z= factor(c(rep(1, 15), rep(2, 5)))) %>%
+   ggplot(aes(x = x, y = y, shape = z, size = z, color = z)) +
+   scale_target() +
+   geom_point()
+
+ggsave("example.png")
+```
+
+![plot](./example.png)
