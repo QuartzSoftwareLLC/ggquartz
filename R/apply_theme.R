a5a30166c74085b9c794329a175fa6ad21b5d0f2
Jacob Clarke <jacobclarke718@gmail.com>
2022-05-06 07:18:15 -0500

    updated theme to not flat

--- R/apply_theme.R
+++ R/apply_theme.R
@@ -7,5 +7,11 @@ apply_theme <- function() {
     update_geom_defaults("line", list(size = 1.5, alpha = 0.8))
     update_geom_defaults("point", list(size = 2.5))
     update_geom_defaults("area", list(alpha = 0.7))
-    ggthemr::ggthemr("flat")
+    ggthemr::ggthemr("fresh")
+    original <- ggthemr::swatch()
+    ggthemr::lighten_swatch(amount = 0.2)
+    second <- ggthemr::swatch()
+    ggthemr::lighten_swatch(amount = 0.2)
+    third <- ggthemr::swatch()
+    ggthemr::ggthemr(ggthemr::define_palette(swatch = unique(c(original, second, third)), gradient = c(lower = "red", upper = "green")))
 }
